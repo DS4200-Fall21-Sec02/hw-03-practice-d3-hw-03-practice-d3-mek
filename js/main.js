8ea534547574992fd9e0e54577520573a1b59903
rexzhang66 <72946512+rexzhang66@users.noreply.github.com>
2021-09-29 16:49:13 -0400

    change1

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ let margin = {
   width = 500 - margin.left - margin.right,
   height = 500 - margin.top - margin.bottom;
 
+
 // first visualization
 let svg1 = d3.select('#vis1')
   .append('svg')
@@ -17,6 +18,63 @@ let svg1 = d3.select('#vis1')
   .attr('width', '100%') // this is now required by Chrome to ensure the SVG shows up at all
   .style('background-color', '#ccc') // change the background color to light gray
   .attr('viewBox', [0, 0, width + margin.left + margin.right, height + margin.top + margin.bottom].join(' '))
+
+
+// Parse the Data
+d3.csv("/data/OlympicMedals.csv", function(data) {
+
+  // List of subgroups = header of the csv files = soil condition here
+  var subgroups = data.columns.slice(2)
+
+  // List of groups = species here = value of the first column called group -> I show them on the X axis
+  var countries = d3.map(data, function(d){return(d.country)}).keys()
+
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(countries)
+      .range([0, width])
+      .padding([0.2])
+  svg1.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(0));
+
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 50])
+    .range([ height, 0 ]);
+  svg1.append("g")
+    .call(d3.axisLeft(y));
+
+  // Another scale for subgroup position?
+  var xSubgroup = d3.scaleBand()
+    .domain(subgroups)
+    .range([0, x.bandwidth()])
+    .padding([0.05])
+
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    .range(['#ffd700','#c0c0c0','#cd7f32'])
+
+  // Show the bars
+  svg1.append("g")
+    .selectAll("g")
+    // Enter in data = loop group per group
+    .data(data)
+    .enter()
+    .append("g")
+      .attr("transform", function(d) { return "translate(" + x(d.country) + ",0)"; })
+    .selectAll("rect")
+    .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+    .enter().append("rect")
+      .attr("x", function(d) { return xSubgroup(d.key); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("width", xSubgroup.bandwidth())
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("fill", function(d) { return color(d.key); });
+
+})
+
 
 // second visualization
 let svg2 = d3.select('#vis2')
