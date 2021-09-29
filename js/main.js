3e3dc7be6941b5fc1df4d9ae7863802f469ca7a5
rexzhang66 <72946512+rexzhang66@users.noreply.github.com>
2021-09-29 17:44:03 -0400

    change2

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ let margin = {
     right: 30,
     bottom: 35
   },
-  width = 500 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+  width = 2000 - margin.left - margin.right,
+  height = 2000 - margin.top - margin.bottom;
 
 
 // first visualization
@@ -21,20 +21,32 @@ let svg1 = d3.select('#vis1')
 
 
 // Parse the Data
-d3.csv("/data/OlympicMedals.csv", function(data) {
+let census = d3.csv("data/OlympicMedals.csv")
+census.then(function(data) {
 
   // List of subgroups = header of the csv files = soil condition here
-  var subgroups = data.columns.slice(2)
+  var subgroups = data.columns.slice(1)
 
   // List of groups = species here = value of the first column called group -> I show them on the X axis
-  var countries = d3.map(data, function(d){return(d.country)}).keys()
+  var countries = new Map(data, function(d){return(d.country)}).keys()
+
+// SVG
+  var svg = svg1.append('svg')
+      .attr('height',height + margin.top + margin.bottom)
+      .attr('width',width + margin.left + margin.right)
+    .append('g')
+      .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
+
+
+
+
 
   // Add X axis
   var x = d3.scaleBand()
       .domain(countries)
       .range([0, width])
       .padding([0.2])
-  svg1.append("g")
+  svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x).tickSize(0));
 
@@ -42,7 +54,7 @@ d3.csv("/data/OlympicMedals.csv", function(data) {
   var y = d3.scaleLinear()
     .domain([0, 50])
     .range([ height, 0 ]);
-  svg1.append("g")
+  svg.append("g")
     .call(d3.axisLeft(y));
 
   // Another scale for subgroup position?
@@ -57,7 +69,7 @@ d3.csv("/data/OlympicMedals.csv", function(data) {
     .range(['#ffd700','#c0c0c0','#cd7f32'])
 
   // Show the bars
-  svg1.append("g")
+  svg.append("g")
     .selectAll("g")
     // Enter in data = loop group per group
     .data(data)
